bfd957739c894837e5d5eed46ad045cbcacbba43
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2022-09-04 23:05:32 +0300

    Добавить отложенную загрузку картинки и текста

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,46 @@
-import { 
-    imageLinks,
-    contentButton,
-    contentImage,
-    contentText
- } from "./data.js";
+import {
+  imageLinks,
+  contentButton,
+  contentImage,
+  contentText,
+  contentWaitText,
+} from "./data.js";
 
- function getRandomImageLink(){
-    const index = Math.floor(Math.random() * imageLinks.length);
-    return imageLinks[index];
- }
+function getRandomImageLink() {
+  const index = Math.floor(Math.random() * imageLinks.length);
+  return imageLinks[index];
+}
 
- function getQuote(){
-    fetch("https://api.kanye.rest")
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            contentText.textContent = data.quote;
-        })
-        .catch(() => {
-            contentText.textContent = "No words today...";
-        })
- }
+function renderLoading(isLoading) {
+  if (isLoading) {
+    contentImage.classList.add("content__image_hidden");
+    contentWaitText.classList.add("content__wait-text_visible");
+  } else {
+    contentImage.classList.remove("content__image_hidden");
+    contentWaitText.classList.remove("content__wait-text_visible");
+  }
+}
 
- contentButton.addEventListener("click", () => {
-    contentImage.src = getRandomImageLink();
-    getQuote();
- });
+function getQuote() {
+  renderLoading(true);
+  setPicture();
+  fetch("https://api.kanye.rest")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      contentText.textContent = data.quote;
+    })
+    .catch(() => {
+      contentText.textContent = "No words today...";
+    })
+    .finally(() => {
+      renderLoading(false);
+    });
+}
 
+function setPicture() {
+  contentImage.src = getRandomImageLink();
+}
+
+contentButton.addEventListener("click", getQuote);
